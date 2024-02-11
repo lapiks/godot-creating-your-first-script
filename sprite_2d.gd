0235e0e52f74988dcdcdf000583f142e40caf7eb
lapiks <dhenin.julien@gmail.com>
2024-02-11 14:47:27 +0100

    Toggle motion button

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -2,11 +2,13 @@ extends Sprite2D
 
 var speed = 400
 var angular_speed = PI
+var auto_motion = false
 
 
 func _process(delta):
 	var direction = (
-		-1 if Input.is_action_pressed("ui_left")
+		1 if auto_motion
+		else -1 if Input.is_action_pressed("ui_left")
 		else 1 if Input.is_action_pressed("ui_right")
 		else 0
 	)
@@ -14,9 +16,13 @@ func _process(delta):
 	rotation += angular_speed * direction * delta
 	
 	var velocity = (
-		Vector2.UP.rotated(rotation) * speed if Input.is_action_pressed("ui_up")
+		Vector2.UP.rotated(rotation) * speed if Input.is_action_pressed("ui_up") or auto_motion
 		else Vector2.DOWN.rotated(rotation) * speed if Input.is_action_pressed("ui_down")
 		else Vector2.ZERO
 	)
 	
 	position += velocity * delta
+
+
+func _on_button_pressed():
+	auto_motion = not auto_motion
